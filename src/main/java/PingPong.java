45cdf5f1830be4c992f67fec0566895f99b9e4c1
Ariel Spear and Momo Ozawa <student@epicodus.com>
2015-08-10 10:40:57 -0700

    Add count test

---
+++ src/main/java/PingPong.java
@@ -0,0 +1,13 @@
+import java.io.Console;
+import java.util.ArrayList;
+
+public class PingPong {
+  public static void main(String[] args) {
+
+  }
+
+  public ArrayList<Integer> playPingPong(int myNumber){
+      ArrayList<Integer> result = new ArrayList<Integer>();
+      return result;
+  }
+}
