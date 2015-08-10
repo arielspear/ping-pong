741e3ff538de1723fe4eddb1ba41eec0184b765b
Ariel Spear and Momo Ozawa <student@epicodus.com>
2015-08-10 11:23:35 -0700

    Pass all counting tests

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -8,6 +8,19 @@ public class PingPong {
 
   public ArrayList<Object> playPingPong(int myNumber){
       ArrayList<Object> result = new ArrayList<Object>();
+
+      for (int i=1; i<=myNumber; i++) {
+
+          if ( i % 15 == 0){
+              result.add("Ping-Pong");
+          } else if (i % 3 == 0){
+              result.add("Ping");
+          } else if (i % 5 == 0) {
+              result.add("Pong");
+          } else {
+              result.add(i);
+          }
+      }
       return result;
   }
 }
