8bf48aa33f3415eb795b85e596d5cb086e2ba034
Ariel Spear and Momo Ozawa <student@epicodus.com>
2015-08-10 10:56:17 -0700

    Add fifteen test

--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -35,4 +35,26 @@ public class PingPongTest {
         assertEquals(expected, testPingPong.playPingPong(5));
     }
 
+    @Test
+    public void playPingPong_pingPongForMultiplesOfFifteen_pingPong() {
+        PingPong testPingPong = new PingPong();
+        ArrayList<Object> expected =  new ArrayList<Object>();
+        expected.add(1);
+        expected.add(2);
+        expected.add("Ping");
+        expected.add(4);
+        expected.add("Pong");
+        expected.add("Ping");
+        expected.add(7);
+        expected.add(8);
+        expected.add("Ping");
+        expected.add("Pong");
+        expected.add(11);
+        expected.add("Ping");
+        expected.add(13);
+        expected.add(14);
+        expected.add("Ping-Pong");
+        assertEquals(expected, testPingPong.playPingPong(15));
+    }
+
 }
