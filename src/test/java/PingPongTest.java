5eb9e3911ffa8252a580b181438999c943ae2dbe
Ariel Spear and Momo Ozawa <student@epicodus.com>
2015-08-10 10:39:46 -0700

    Add count test

--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -1,7 +1,15 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.ArrayList;
 
 public class PingPongTest {
     @Test
-    public void 
+    public void playPingPong_countsToNumber_correctArrayList() {
+        PingPong testPingPong = new PingPong();
+        ArrayList<Integer> expected =  new ArrayList<Integer>();
+        expected.add(1);
+        expected.add(2);
+        assertEquals(expected, testPingPong.playPingPong(2));
+    }
+
 }
